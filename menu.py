229ada643b11438df75ebb0fe551dfa28878c7c9
Kile Deal <kile@kjdeal.dev>
2020-11-21 02:35:24 -0500

    added some docstrings to Menu added some methods to make steps in get_action() easier to read reworked options and removed opt_dict

--- menu.py
+++ menu.py
@@ -1,7 +1,14 @@
 from action import Action
+from typing import TypeVar
+
+
+MenuOption = TypeVar("MenuOption", "Menu", Action)
 
 
 class Menu:
+    """Class for constructing the text menus. Uses builder pattern to allow
+    for method chaining style of construction."""
+
     def __init__(self, title: str = "", prompt: str = "What will you do? "):
         self.title = title
         self.prompt = prompt
@@ -19,20 +26,28 @@ class Menu:
         return self
 
     @property
-    def options(self):
-        return self.actions + self.submenus
+    def options(self) -> dict[str, MenuOption]:
+        opts = self.actions + self.submenus
+        return {f"{i+1}": opt for i, opt in enumerate(opts)}
 
-    @property
-    def opt_dict(self):
-        return {f"{i+1}": opt for i, opt in enumerate(self.options)}
+    def valid_selection(self, selection: str) -> bool:
+        return selection in self.options.keys()
+
+    def get_selection(self, selection: str) -> MenuOption:
+        return self.options[selection]
+
+    def is_menu(self, selection: MenuOption) -> bool:
+        return isinstance(selection, self.__class__)
 
     def print_separator(self):
         print("-" * 25)
 
     def show(self):
-        if self.is_child: 
+        """Does the work of printing out the menu text"""
+
+        if self.is_child:
             print()
-        
+
         self.print_separator()
 
         print(f"{self.title}")
@@ -40,34 +55,40 @@ class Menu:
         if self.title != "":
             print()
 
-        for i, opt in enumerate(self.options):
+        for key, opt in self.options.items():
             if isinstance(opt, self.__class__):
                 text = opt.title.title()
             else:
                 text = str(opt)
-            print(f"{i+1}) {text}")
+            print(f"{key}) {text}")
 
         if self.is_child:
             print("0) Cancel")
 
         print()
 
-
     def get_action(self) -> str:
+        """Shows the menu text and prompts the user for input.
+        Will return the name of an Action to be performed in the battle loop"""
+
         self.show()
 
         while True:
-            selection = input(self.prompt)
-            if self.is_child and selection == "0":
+            select_in = input(self.prompt)
+            if self.is_child and select_in == "0":
                 return
-            elif selection not in self.opt_dict.keys():
+            elif not self.valid_selection(select_in):
                 continue
-            elif isinstance(self.opt_dict[selection], self.__class__):
-                action_name = self.opt_dict[selection].get_action()
+            elif self.is_menu(
+                # Using 3.8's fancy new walrus operator
+                selection := self.get_selection(select_in)
+            ):
+                # For when the selection is a submenu
+                action_name = selection.get_action()
                 if action_name:
                     return action_name
                 else:
                     self.show()
                     continue
             else:
-                return self.opt_dict[selection].name
+                return selection.name
